ccc815923c59db881317d839b8e4cb053f4911cf
carlos augusto romero orozco <65257318+Carlos1kaiser1Romero@users.noreply.github.com>
2022-06-30 23:18:27 -0400

    FEAT:Admin frontend

---
+++ CIAcademicApp/Pages/Administrador/ConsultarDocenteAdministrador.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIAcademicApp.Pages.Administrador
+{
+    public class ConsultarDocenteAdministradorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CIAcademicApp/Pages/Administrador/CrearNuevoUsuarioAdministrador.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CIAcademicApp.Pages.Administrador
+{
+    public class CrearNuevoUsuarioAdministradorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
